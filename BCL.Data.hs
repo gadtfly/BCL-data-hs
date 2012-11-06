70df1b098c916efb5a907bd454a05a8c5df3ab12
Adam Louis <adamlouis@gmail.com>
2012-11-06 13:52:06 -0800

    Add rudimentary error-handling (probably unidiomatic), and dirty hack to locally cache Product pages

--- BCL.Data.hs
+++ BCL.Data.hs
@@ -1,4 +1,6 @@
+
 module BCL.Data where
+import qualified Control.Exception as Exception
 import qualified Text.Parsec.Language as Language
 import qualified Text.Parsec.Token as Token
 import Text.Parsec hiding ((<|>), many)
@@ -7,80 +9,112 @@ import Network.Curl.Download
 import Control.Applicative
 import Control.Monad
 import Data.List
+import Debug.Trace
 
 -- Constants
-perPage    = 1000                             :: Integer
-useLocal   = True                             :: Bool     -- refactor for `useCached`?
-localBase  = "cached/"                        :: FilePath
-remoteBase = "http://www.bcliquorstores.com/" :: URL
-productCatalogueBase = "product-catalogue"    :: Path
+
+perPage       = 1000                              :: Integer
+useCached     = True                              :: Bool
+cachePath     = "cached/"                         :: String
+urlPath       = "http://www.bcliquorstores.com/"  :: String
+cataloguePath = "product-catalogue"               :: String
+productPath   = "product/"                        :: String
 
 
--- Type synonyms for readiblity
-type Path = String
-type URL  = String
-type Page = String
-type SKU  = Integer
+
+-- Product Details
+
+data Product = Product
+
 
 
 -- Parsers: Product Catalogue
-productCount :: Parser Integer
-productCount = decimal <* string " Matches"
 
-sku :: Parser SKU
+matches :: Parser Integer
+matches = decimal <* string " Matches"
+
+sku :: Parser Integer
 sku = string "/product/" *> decimal
 
-pageSKUs :: Parser [SKU]
+pageSKUs :: Parser [Integer]
 pageSKUs = nub <$> many (try (skipTo sku))
 
 
+
 -- Parsers: Utility
+
 decimal :: Parser Integer
 decimal = Token.decimal (Token.makeTokenParser Language.emptyDef)
 
 skipTo :: Parser a -> Parser a
-skipTo p = try p <|> anyChar *> skipTo p
-
-getParse :: Parser a -> String -> a
-getParse p = either (error . show) id . parse p ""
+skipTo p = try p <|> anyToken *> skipTo p
 
 
--- IO Utilities
--- Refactor with Arrows?
-getLocal :: FilePath -> IO Page
-getLocal = readFile
 
-getRemote :: URL -> IO Page
-getRemote = either fail return <=< openURIString
+-- Main Utilities
 
-getPage :: Path -> IO Page
-getPage path | useLocal  = getLocal (localBase ++ path)
-             | otherwise = do
-                  page <- getRemote (remoteBase ++ path)
-                  writeFile (localBase ++ path) page
+catalogueQuery :: Integer -> Integer -> String
+catalogueQuery x n = cataloguePath ++ "?perPage=" ++ show x ++ "&page=" ++ show n
+
+productQuery :: Integer -> String
+productQuery n = productPath ++ show n
+
+getCached :: String -> IO String
+getCached path = readFile (cachePath ++ path)
+
+updateCached :: String -> String -> IO ()
+updateCached path page = writeFile (cachePath ++ path) page
+
+getUrl :: String -> IO String
+getUrl path = either fail return =<< openURIString (urlPath ++ path)
+
+getPage :: String -> IO String
+getPage path  | useCached = getCached path
+              | otherwise = do
+                  page <- getUrl path
+                  updateCached path page
                   return page
 
-parsePage :: Path -> Parser a -> IO a
-parsePage path parser = getParse parser <$> getPage path
+handler :: a -> Exception.SomeException -> IO a
+handler x e = traceShow e (return x)
 
 
 -- Main
-productCatalogue :: Integer -> Integer -> Path
-productCatalogue perPage page = productCatalogueBase ++ "?perPage=" ++ (show perPage) ++ "&page=" ++ (show page)
 
-getProductCount :: IO Integer
-getProductCount = parsePage (productCatalogue 1 0) (skipTo productCount)
+getMatches :: IO Integer
+getMatches = do
+  page <- getPage (catalogueQuery 1 0)
+  return $ either (error . show) id $ parse (skipTo matches) "getMatches" page
 
-getPageSKUs :: Integer -> IO [SKU]
-getPageSKUs page = parsePage (productCatalogue perPage page) pageSKUs
+getPageSKUs :: String -> IO [Integer]
+getPageSKUs path = do
+  page <- Exception.catch (getPage path) (handler "")
+  return $ either (error . (++ ": " ++ path) . show) id $ parse pageSKUs ("getPageSKUs" ++ path) page
 
-getSKUs :: IO [SKU]
-getSKUs = do
-  count <- getProductCount
-  let pages = ceiling (fromIntegral count / fromIntegral perPage)
-  concat <$> mapM getPageSKUs (enumFromTo 0 (pages-1))
+getSKUs :: Integer -> IO [Integer]
+getSKUs matches = do
+  let pageTotal   = ceiling $ fromIntegral matches / fromIntegral perPage
+      pageNumbers = enumFromTo 0 (pageTotal - 1)
+      paths       = map (catalogueQuery perPage) pageNumbers
+  concat <$> mapM getPageSKUs paths
 
-main :: IO ()
+getProduct :: Integer -> IO Product
+getProduct sku = do
+  page <- Exception.catch (getPage (productQuery sku)) (handler "")
+  return Product
+
+getProducts :: [Integer] -> IO [Product]
+getProducts skus = do
+  mapM getProduct skus
+
+
+
 main = do
-  print . length =<< getSKUs
-  print "Success"
+  putStrLn "Starting"
+  matches <- getMatches
+  putStrLn $ "Matches: " ++ show matches
+  skus <- getSKUs matches
+  putStrLn $ "SKUs found: " ++ show (length skus)
+  products <- getProducts skus
+  putStrLn $ "Products found: " ++ show (length products)
+  putStrLn "Success"
