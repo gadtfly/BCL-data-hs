ebe36914ed61cfa58bab8b49fdbaf2d20450d848
Adam Louis <adamlouis@gmail.com>
2012-11-15 15:44:53 -0800

    Prepare to abandon page scraping

--- BCL.Data.hs
+++ BCL.Data.hs
@@ -7,8 +7,8 @@ import Text.Parsec.String
 import Network.Curl.Download
 import Control.Applicative
 import Control.Error
+import Control.Proxy
 import Data.List
-import Control.Monad
 
 -- Constants
 
@@ -94,7 +94,7 @@ getMatches :: Script Integer
 getMatches = getPage (catalogueQuery 1 0) >>= getParse (skipTo matches)
 
 getPageSKUs :: String -> Script [Integer]
-getPageSKUs = withDefault [] . getPage >=> getParse pageSKUs
+getPageSKUs path = withDefault [] $ getPage path >>= getParse pageSKUs
 
 getSKUs :: Integer -> Script [Integer]
 getSKUs matches = concat <$> mapM getPageSKUs paths
@@ -103,11 +103,16 @@ getSKUs matches = concat <$> mapM getPageSKUs paths
     pageNumbers = enumFromTo 0 $ totalPages - 1
     paths       = map (catalogueQuery perPage) pageNumbers
 
+--getProducts :: [Integer] -> Script [()]
+--getProducts skus = mapM (getPage . productQuery) skus
+
 main :: IO ()
 main = do
   errLn "Starting"
-  matches <- runScript $ getMatches
+  matches  <- runScript $ getMatches
   errLn $ "Matches: " ++ show matches
-  skus    <- runScript $ getSKUs matches
+  skus     <- runScript $ getSKUs matches
   errLn $ "SKUs: " ++ show (length skus)
+  --products <- runScript $ getProducts skus
+  --errLn $ "Products: " ++ show (length products)
   errLn "Success"
